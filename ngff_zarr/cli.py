7724cc5f6b960eec5cfdc78e1a3e317c6d9a0ec1
Matt McCormick <matt.mccormick@kitware.com>
2022-12-12 21:34:18 -0500

    ENH: Add fsspec support to the CLI copy

--- ngff_zarr/cli.py
+++ ngff_zarr/cli.py
@@ -4,19 +4,32 @@
 
 import argparse
 import sys
-from typing import List, Optional
+from typing import List, Optional, Dict
 import zarr
 from rich.console import Console
 from rich.theme import Theme
 from rich.style import Style
 from rich.highlighter import RegexHighlighter
+from rich import print as rprint
+import json
 
-def open_store(identifier: str, writable: bool=False) -> zarr.storage.StoreLike:
+def open_store(identifier: str, storage_options: Dict, writable: bool=False) -> zarr.storage.StoreLike:
     dimension_separator = None
     mode = 'r'
     if writable:
         dimension_separator = '/'
         mode='w'
+
+    protocol = identifier.find('://') != -1
+    if protocol:
+        try:
+            import fsspec
+        except ImportError:
+            rprint('[italic red]Please install fsspec to access protocols[/italic red]')
+            sys.exit(1)
+        
+        store = zarr.storage.FSStore(identifier, dimension_separator=dimension_separator, normalize_keys=False, mode=mode, **storage_options)
+        return store
 
     if identifier.endswith('.zip'):
         store = zarr.storage.ZipStore(identifier, mode=mode, dimension_separator=dimension_separator)
@@ -27,8 +40,9 @@ def open_store(identifier: str, writable: bool=False) -> zarr.storage.StoreLike:
 
 def copy(args: argparse.Namespace) -> None:
     """Copy the Zarr store args.source to args.dist"""
-    source = open_store(args.source, False)
-    dest = open_store(args.dest, True)
+    storage_options = json.loads(args.storage_options)
+    source = open_store(args.source, storage_options, False)
+    dest = open_store(args.dest, storage_options, True)
     class CopyHighlighter(RegexHighlighter):
         base_style = 'copy.'
         highlights = [
@@ -53,12 +67,12 @@ def copy(args: argparse.Namespace) -> None:
     logcallable = lambda x: console.log(copy_highlighter(x))
     console.log('[i]Starting copy...[/i]')
     with console.status(f"[bold green]Copying {args.source} to {args.dest}...") as status:
-      zarr.convenience.copy_store(source, dest, if_exists='replace',
-              dry_run=args.dry_run, log=logcallable)
+        zarr.convenience.copy_store(source, dest, if_exists='replace',
+                dry_run=args.dry_run, log=logcallable)
     console.log('[i]Copy finished.[/i]')
 
 def main(args: Optional[List[str]] = None) -> None:
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=__doc__)
 
     parser.add_argument(
         "-q",
@@ -74,7 +88,9 @@ def main(args: Optional[List[str]] = None) -> None:
     parser_copy.add_argument("source")
     parser_copy.add_argument("dest")
     parser_copy.add_argument("--dry-run", action="store_true",
-        help="Run a dry run that does not actually copy the store")
+        help="execute a dry run that does not actually copy the store")
+    parser_copy.add_argument("--storage-options", default='{}',
+        help="string of JSON storage_options to pass to fsspec")
     parser_copy.set_defaults(func=copy)
 
     if args is None:
